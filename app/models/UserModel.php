d6637c0aab5389f167d1b4349bffcfef37588e9b
Noumsmam <noumsrasolondraibe@gmail.com>
2025-12-17 21:46:07 +0300

    a revoir

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -4,27 +4,27 @@
     use Flight;
 
     Class UserModel {
-        
-        public function getUsers() {
-            $users = [
-                [ 'id' => 1, 'name' => 'Bob Jones', 'email' => 'bob@example.com' ],
-                [ 'id' => 2, 'name' => 'Bob Smith', 'email' => 'bsmith@example.com' ],
-                [ 'id' => 3, 'name' => 'Suzy Johnson', 'email' => 'suzy@example.com' ],
-                [ 'id' => 4, 'name' => 'Nomena Maminiaina', 'email' => 'nomena@email.com'],
-                [ 'id' => 5, 'name' => 'Henika Maminiaina', 'email' => 'henika@email.com'],
-            ];
-            return $users;
+        private $login;
+        private $password;
+
+        public function __construct($login, $password){
+            $this->login=$login;
+            $this->password=$password;    
         }
 
-        public function getUser($id) {
-            $users = [
-                [ 'id' => 1, 'name' => 'Bob Jones', 'email' => 'bob@example.com' ],
-                [ 'id' => 2, 'name' => 'Bob Smith', 'email' => 'bsmith@example.com' ],
-                [ 'id' => 3, 'name' => 'Suzy Johnson', 'email' => 'suzy@example.com' ],
-                [ 'id' => 4, 'name' => 'Nomena Maminiaina', 'email' => 'nomena@email.com'],
-                [ 'id' => 5, 'name' => 'Henika Maminiaina', 'email' => 'henika@email.com'],
-            ];
-            return $users[$id];
+        public function getLogin(){
+            return $this->login;
         }
-    }
 
+        public function getPassword(){
+            return $this->password;
+        }
+
+        public function checkUser(){
+            $db = Flight::db();
+            $STMT=$db->prepare("SELECT * FROM livraison_user WHERE logins = ? AND pwd = ?;");
+            $STMT->execute([$this->login,$this->password]);
+            $result=$STMT->fetch();
+            return $result;
+        }
+}
