ef5151ef063dd12ea4b8d8f4613532fb9b479edb
Noumsmam <noumsrasolondraibe@gmail.com>
2025-12-18 10:52:18 +0300

    livraison debut

---
+++ app/controllers/LivrasionController.php
@@ -0,0 +1,11 @@
+<?php
+    namespace App\Controllers;
+
+    use Flight;
+
+    Class LivrasionController {
+        public function createLivraison() {
+            
+        }
+    }
+?>
--- app/models/LivraisonModel.php
+++ app/models/LivraisonModel.php
@@ -12,8 +12,9 @@
         private $status;
         private $cout;
         private $chiffreAffaire;
+        private $dateLivraison;
 
-        public function __construct($idColis,$idChauffeur,$idVehicule,$idEntrepot,$adresse,$status,$cout,$chiffreAffaire) {
+        public function __construct($idColis,$idChauffeur,$idVehicule,$idEntrepot,$adresse,$status,$cout,$chiffreAffaire,$dateLivraison) {
             $this->idColis=$idColis;
             $this->idChauffeur=$idChauffeur;
             $this->idVehicule=$idVehicule;
@@ -22,6 +23,7 @@
             $this->status=$status;
             $this->cout=$cout;
             $this->chiffreAffaire=$chiffreAffaire;
+            $this->dateLivraison=$dateLivraison;
         }
 
         public function getId() {
@@ -60,5 +62,16 @@
             return $this->chiffreAffaire;
         }
 
+        public function getDateLivraison(){
+            return $this->dateLivraison;
+        }
+
+        public function saveLivraison(){
+            $db=Flight::db();
+            $STMT = $db->prepare("INSERT INTO livraison_livraison
+            (colis_id,chauffeur_id,vehicule_id,entrepot_id,adresse_destination,status_id,date_livraison,cout_revient,chiffre_affaire) 
+            VALUES (?,?,?,?,?,?,?,?,?);");
+            $STMT->execute([$this->getIdColis(),$this->getIdVehicule(),$this->getIdEntrepot(),$this->getAdresse(),$this->getDateLivraison(),$this->getCout(),$this->getChiffreAffaire()]);
+        }
     }
 ?>
